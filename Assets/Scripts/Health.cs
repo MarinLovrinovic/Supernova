fc48a1fb5e9b669d52982ebf970dee2145cfa970
fran <fran.androic@fer.hr>
2026-01-13 16:24:57 +0100

    update Projectile i Health skripte, dodani prefabovi za upgradeove

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,102 +4,50 @@ using Fusion;
 using Fusion.Addons.Physics;
 
 public class Health : NetworkBehaviour {
-	
-	public enum deathAction {loadLevelWhenDead,doNothingWhenDead, destroyedWhenDead};
-	public float healthPoints = 20f;
-	public float respawnHealthPoints = 20f;		//base health points
-	public int numberOfLives = 1;					//lives and variables for respawning
-	public bool isAlive = true;	
-	public GameObject explosionPrefab;
-	public deathAction onLivesGone = deathAction.doNothingWhenDead;
-	public string LevelToLoad = "";
-	private Vector3 respawnPosition;
-	private Quaternion respawnRotation;
-	private Rigidbody2D rb;
+
+    public enum deathAction { doNothingWhenDead, destroyedWhenDead };
+
+    public float healthPoints = 20f;
+    public float respawnHealthPoints = 20f;
+    public int numberOfLives = 1;
+    public bool isAlive = true;
+    public deathAction onLivesGone = deathAction.doNothingWhenDead;
+    private Rigidbody2D rb;
 
     public override void Spawned()
     {
-        // store initial position as respawn location
-        respawnPosition = transform.position;
-        respawnRotation = transform.rotation;
         rb = GetComponent<Rigidbody2D>();
-
-        // Debug.Log("This is health of:  " + rb.gameObject.name);
-
-        if (LevelToLoad == "") // default to current scene 
-        {
-            LevelToLoad = Application.loadedLevelName;
-        }
     }
-    void Start () 
-	{
-		// store initial position as respawn location
-		respawnPosition = transform.position;
-		respawnRotation = transform.rotation;
-		rb = GetComponent<Rigidbody2D>();
-		
-		// Debug.Log("This is health of:  " + rb.gameObject.name);
-		
-		if (LevelToLoad=="") // default to current scene 
-		{
-			LevelToLoad = Application.loadedLevelName;
-		}
-	}
 
-    // Update is called once per frame
     public override void FixedUpdateNetwork()
     {
-		if (healthPoints <= 0) {				// if the object is 'dead'
-			numberOfLives--;					// decrement # of lives, update lives GUI
-			
-			if (explosionPrefab!= null) {
-				Instantiate (explosionPrefab, transform.position, Quaternion.identity);
-			}
-			
-			if (numberOfLives > 0) { // respawn
-				transform.position = respawnPosition;	// reset the player to respawn position
-				transform.rotation = respawnRotation;
-				healthPoints = respawnHealthPoints;	// give the player full health again
-			} else { // here is where you do stuff once ALL lives are gone)
-				isAlive = false;
-				
-				switch(onLivesGone)
-				{ 
-				case deathAction.destroyedWhenDead:
-					Destroy(rb);
-					break;
-				case deathAction.loadLevelWhenDead:
-					Application.LoadLevel (LevelToLoad);
-					break;
-				case deathAction.doNothingWhenDead:
-					// do nothing, death must be handled in another way elsewhere
-					break;
-				}
-				Destroy(gameObject);
-			}
-		}
-	}
-	
-	public void ApplyDamage(float amount)
-	{	
-		healthPoints = healthPoints - amount;
-		// Debug.Log("amount of damage applied:  " + amount);
-		// Debug.Log("current health:  " + healthPoints);
-		//
-	}
-	
-	public void ApplyHeal(float amount)
-	{
-		healthPoints = healthPoints + amount;
-	}
+        if (healthPoints <= 0)
+        {
+            numberOfLives--;
 
-	public void ApplyBonusLife(int amount)
-	{
-		numberOfLives = numberOfLives + amount;
-	}
-	
-	public void updateRespawn(Vector3 newRespawnPosition, Quaternion newRespawnRotation) {
-		respawnPosition = newRespawnPosition;
-		respawnRotation = newRespawnRotation;
-	}
+            if (numberOfLives > 0)
+            {
+                healthPoints = respawnHealthPoints;
+            }
+            else
+            {
+                isAlive = false;
+
+                switch (onLivesGone)
+                {
+                    case deathAction.destroyedWhenDead:
+                        Destroy(rb);
+                        break;
+                    case deathAction.doNothingWhenDead:
+                        break;
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void ApplyDamage(float amount)
+    {
+        healthPoints = healthPoints - amount;
+    }
 }
